276738de2b03bd891ca5fe0bb68864da54d35ced
Mas Ben <Mas Ben@MasBen-PC>
2014-09-03 14:41:33 +0700

    03-09-2014-0241PM

--- footer.php
+++ footer.php
@@ -1,8 +1,15 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+</div>
+            </div>
+        </div>
+        <div class="clear">
+        </div>
+    </div>
+    <div class="clear">
+    </div>
+    <div id="site_info">
+        <p>
+            Copyright <a href="#">IT Bell Menteng</a>. All Rights Reserved.
+        </p>
+    </div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,18 +1,6 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        echo"test";
-        ?>
-    </body>
-</html>
+<?php
+    include("header.php");
+?>
+<?php
+    include("footer.php");
+?>
